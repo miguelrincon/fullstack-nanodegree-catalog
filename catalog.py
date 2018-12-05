20a611dc012942ca28b81571d377fd2e53b88617
Miguel Rincon <miguel.rincon86@gmail.com>
2018-12-05 15:29:22 +0800

    Format according to pep8

--- catalog.py
+++ catalog.py
@@ -1,62 +1,76 @@
-from flask import Flask, render_template, request, redirect, jsonify, url_for, flash, make_response
-from scripts.database_setup import Base, User, Category, Item
+import json
+import random
+import string
+import urllib.parse.urlencode as urlencode
+import httplib2
 
 import sqlalchemy
 from sqlalchemy.orm import sessionmaker
 from flask import session as login_session
 
-import json
-import random
-import string
-import urllib.parse
-import httplib2
+from flask import Flask, render_template, request, redirect, \
+    jsonify, url_for, flash, make_response
+from scripts.database_setup import Base, User, Category, Item
+
 
 app = Flask(__name__)
 
 # Connect to Database and create database session
-engine = sqlalchemy.create_engine('sqlite:///data/catalog.db', connect_args={'check_same_thread': False})
+engine = sqlalchemy.create_engine(
+    'sqlite:///data/catalog.db', connect_args={'check_same_thread': False})
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+
 @app.route('/')
 def showHome():
-    categories = session.query(Category).order_by(sqlalchemy.asc(Category.name)).all()
+    categories = session.query(Category).order_by(
+        sqlalchemy.asc(Category.name)).all()
     user = getUserInfo()
     return render_template('home.html', user=user, categories=categories)
 
+
 @app.route('/category/<int:category_id>')
 def showCategory(category_id):
-    categories = session.query(Category).order_by(sqlalchemy.asc(Category.name)).all()
-    category = session.query(Category).filter_by(id = category_id).one()
-    items = session.query(Item).filter_by(category_id = category_id).all()
+    categories = session.query(Category).order_by(
+        sqlalchemy.asc(Category.name)).all()
+    category = session.query(Category).filter_by(id=category_id).one()
+    items = session.query(Item).filter_by(category_id=category_id).all()
     user = getUserInfo()
-    return render_template('category.html', user=user, categories=categories, category=category, items=items)
+    return render_template('category.html', user=user,
+                           categories=categories,
+                           category=category,
+                           items=items)
+
 
 @app.route('/category/<int:category_id>.json')
 def showCategoryJson(category_id):
-    category = session.query(Category).filter_by(id = category_id).one()
+    category = session.query(Category).filter_by(id=category_id).one()
     return jsonify(category.serialize)
+
 
 @app.route('/category/<int:category_id>/items.json')
 def showCategoryItemsJson(category_id):
-    items = session.query(Item).filter_by(category_id = category_id).all()
+    items = session.query(Item).filter_by(category_id=category_id).all()
     itemJson = []
     for i in items:
         itemJson.append(i.serialize)
     return jsonify(itemJson)
+
 
 @app.route('/category/<int:category_id>/create', methods=['GET', 'POST'])
 def createItem(category_id):
     user = getUserInfo()
     if(not user):
         return make_response(
-            render_template('error.html', code=401, message='Please login to create items.'),
+            render_template('error.html', code=401,
+                            message='Please login to create items.'),
             401
         )
     if request.method == 'POST':
-        category = session.query(Category).filter_by(id = category_id).one()
+        category = session.query(Category).filter_by(id=category_id).one()
         item = Item()
         item.name = request.form['name']
         item.description = request.form['description']
@@ -67,37 +81,50 @@ def createItem(category_id):
         flash('"%s" created succesfully!' % item.name)
         return redirect(url_for('showItem', item_id=item.id))
     else:
-        categories = session.query(Category).order_by(sqlalchemy.asc(Category.name)).all()
-        category = session.query(Category).filter_by(id = category_id).one()
-        items = session.query(Item).filter_by(category_id = category.id).all()
-        return render_template('item.create.html', user=user, categories=categories, category=category, items=items)
+        categories = session.query(Category).order_by(
+            sqlalchemy.asc(Category.name)).all()
+        category = session.query(Category).filter_by(id=category_id).one()
+        items = session.query(Item).filter_by(category_id=category.id).all()
+        return render_template('item.create.html', user=user,
+                               categories=categories,
+                               category=category,
+                               items=items)
+
 
 @app.route('/item/<int:item_id>')
 def showItem(item_id):
-    categories = session.query(Category).order_by(sqlalchemy.asc(Category.name)).all()
-    item = session.query(Item).filter_by(id = item_id).one()
+    categories = session.query(Category).order_by(
+        sqlalchemy.asc(Category.name)).all()
+    item = session.query(Item).filter_by(id=item_id).one()
     category = item.category
-    items = session.query(Item).filter_by(category_id = category.id).all()
+    items = session.query(Item).filter_by(category_id=category.id).all()
     user = getUserInfo()
-    return render_template('item.html', user=user, categories=categories, category=category, items=items,item=item)
+    return render_template('item.html', user=user,
+                           categories=categories,
+                           category=category,
+                           items=items, item=item)
+
 
 @app.route('/item/<int:item_id>.json')
 def showItemJson(item_id):
-    item = session.query(Item).filter_by(id = item_id).one()
+    item = session.query(Item).filter_by(id=item_id).one()
     return jsonify(item.serialize)
+
 
 @app.route('/item/<int:item_id>/edit', methods=['GET', 'POST'])
 def editItem(item_id):
     user = getUserInfo()
     if(not user):
         return make_response(
-            render_template('error.html', code=401, message='Please login to delete items.'),
+            render_template('error.html', code=401,
+                            message='Please login to delete items.'),
             401
         )
-    item = session.query(Item).filter_by(id = item_id).one()
+    item = session.query(Item).filter_by(id=item_id).one()
     if(user.id != item.user_id):
         return make_response(
-            render_template('error.html', code=401, message='You can only delete your own items.'),
+            render_template('error.html', code=401,
+                            message='You can only delete your own items.'),
             403
         )
     if request.method == 'POST':
@@ -108,23 +135,31 @@ def editItem(item_id):
         flash('"%s" updated succesfully!' % item.name)
         return redirect(url_for('showItem', item_id=item.id))
     else:
-        categories = session.query(Category).order_by(sqlalchemy.asc(Category.name)).all()
+        categories = session.query(Category).order_by(
+            sqlalchemy.asc(Category.name)).all()
         category = item.category
-        items = session.query(Item).filter_by(category_id = category.id).all()
-        return render_template('item.edit.html', user=user, categories=categories, category=category, items=items,item=item)
+        items = session.query(Item).filter_by(category_id=category.id).all()
+        return render_template('item.edit.html', user=user,
+                               categories=categories,
+                               category=category,
+                               items=items,
+                               item=item)
+
 
 @app.route('/item/<int:item_id>/delete', methods=['GET', 'POST'])
 def deleteItem(item_id):
     user = getUserInfo()
     if(not user):
         return make_response(
-            render_template('error.html', code=401, message='Please login to delete items.'),
+            render_template('error.html', code=401,
+                            message='Please login to delete items.'),
             401
         )
-    item = session.query(Item).filter_by(id = item_id).one()
+    item = session.query(Item).filter_by(id=item_id).one()
     if(user.id != item.user_id):
         return make_response(
-            render_template('error.html', code=401, message='You can only delete your own items.'),
+            render_template('error.html', code=401,
+                            message='You can only delete your own items.'),
             403
         )
     if request.method == 'POST':
@@ -134,43 +169,59 @@ def deleteItem(item_id):
         flash('"%s" deleted succesfully!' % item.name)
         return redirect(url_for('showCategory', category_id=category_id))
     else:
-        categories = session.query(Category).order_by(sqlalchemy.asc(Category.name)).all()
+        categories = session.query(Category).order_by(
+            sqlalchemy.asc(Category.name)).all()
         category = item.category
-        items = session.query(Item).filter_by(category_id = category.id).all()
-        return render_template('item.delete.html', user=user, categories=categories, category=category, items=items,item=item)
+        items = session.query(Item).filter_by(category_id=category.id).all()
+        return render_template('item.delete.html', user=user,
+                               categories=categories, category=category,
+                               items=items, item=item)
+
 
 @app.route('/login')
 def showLogin():
-    categories = session.query(Category).order_by(sqlalchemy.asc(Category.name)).all()
+    categories = session.query(Category).order_by(
+        sqlalchemy.asc(Category.name)).all()
 
-    state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(32))
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for x in range(32))
     login_session['state'] = state
 
-    app_id = json.loads(open('config/github_secrets.json', 'r').read())['web']['app_id']
-    github_authorize_url = 'https://github.com/login/oauth/authorize?' + urllib.parse.urlencode({
-        'client_id': app_id,
-        'state': state,
-        'scope': 'user',
-        'redirect_uri': 'http://localhost:5001/github-callback'
-    })
-    return render_template('login.html', user=getUserInfo(), github_authorize_url=github_authorize_url, categories=categories)
+    app_id = json.loads(open('config/github_secrets.json',
+                             'r').read())['web']['app_id']
+    github_authorize_url = 'https://github.com/login/oauth/authorize?' + \
+        urlencode({
+            'client_id': app_id,
+            'state': state,
+            'scope': 'user',
+            'redirect_uri': 'http://localhost:5001/github-callback'
+        })
+    return render_template('login.html', user=getUserInfo(),
+                           github_authorize_url=github_authorize_url,
+                           categories=categories)
+
 
 @app.route('/logout')
 def logout():
     access_token = login_session['github_access_token']
-    app_id = json.loads(open('config/github_secrets.json', 'r').read())['web']['app_id']
-    app_secret = json.loads(open('config/github_secrets.json', 'r').read())['web']['app_secret']
+    app_id = json.loads(open('config/github_secrets.json',
+                             'r').read())['web']['app_id']
+    app_secret = json.loads(
+        open('config/github_secrets.json', 'r').read())['web']['app_secret']
 
-    github_access_token_revoke_url = 'https://api.github.com/applications/%s/tokens/%a' % (app_id, access_token)
+    github_access_token_revoke_url = \
+        'https://api.github.com/applications/%s/tokens/%a' % (
+            app_id, access_token)
     h = httplib2.Http()
     h.add_credentials(app_id, app_secret)
     (revoke_response, revoke_content) = h.request(
-        github_access_token_revoke_url, 
+        github_access_token_revoke_url,
         method='DELETE',
         headers={'Accept': 'application/json'})
     if revoke_response.get('error') is not None:
         response = make_response(
-            render_template('error.html', code=500, message=revoke_response.get('error')),
+            render_template('error.html', code=500,
+                            message=revoke_response.get('error')),
             500
         )
         return response
@@ -182,62 +233,77 @@ def logout():
     flash('You have been logged out.')
     return redirect(url_for('showHome'))
 
+
 @app.route('/github-callback')
 def githubCallback():
     code = request.args.get('code')
     state = request.args.get('state')
     if (request.args.get('state') != login_session['state']):
         response = make_response(
-            render_template('error.html', code=401, message='Invalid state parameter.'),
+            render_template('error.html', code=401,
+                            message='Invalid state parameter.'),
             401
         )
         return response
 
-    app_id = json.loads(open('config/github_secrets.json', 'r').read())['web']['app_id']
-    app_secret = json.loads(open('config/github_secrets.json', 'r').read())['web']['app_secret']
+    app_id = json.loads(open('config/github_secrets.json',
+                             'r').read())['web']['app_id']
+    app_secret = json.loads(
+        open('config/github_secrets.json', 'r').read())['web']['app_secret']
 
-    github_access_token_url = 'https://github.com/login/oauth/access_token?' + urllib.parse.urlencode({
-        'client_id': app_id,
-        'client_secret': app_secret,
-        'code': code,
-        'redirect_uri': 'http://localhost:5001/github-callback',
-        'state': state
-    })
+    github_access_token_url = \
+        'https://github.com/login/oauth/access_token?' + urlencode({
+            'client_id': app_id,
+            'client_secret': app_secret,
+            'code': code,
+            'redirect_uri': 'http://localhost:5001/github-callback',
+            'state': state
+        })
     h = httplib2.Http()
 
     (token_response, token_content) = h.request(
-        github_access_token_url, 
+        github_access_token_url,
         method='POST',
         headers={'Accept': 'application/json'})
     if token_response.get('error') is not None:
         response = make_response(
-            render_template('error.html', code=500, message=token_response.get('error')),
+            render_template('error.html', code=500,
+                            message=token_response.get('error')),
             500
         )
         return response
     if 'access_token' not in json.loads(token_content):
         response = make_response(
-            render_template('error.html', code=401, message='Cannot connect to github, please try again later.'),
+            render_template(
+                'error.html',
+                code=401,
+                message='Cannot connect to github, please try again later.'),
             401
         )
         return response
 
-    login_session['github_access_token'] = json.loads(token_content)['access_token']
-    
-    github_api_url = 'https://api.github.com/user' 
+    login_session['github_access_token'] = json.loads(token_content)[
+        'access_token']
+
+    github_api_url = 'https://api.github.com/user'
+    auth_header = 'token ' + login_session['github_access_token']
     (user_response, user_content) = h.request(
-        github_api_url, 
+        github_api_url,
         method='GET',
-        headers={'Accept': 'application/json', 'Authorization': 'token ' + login_session['github_access_token']})
+        headers={'Accept': 'application/json', 'Authorization': auth_header})
     user_result = json.loads(user_content)
-    if user_result['email'] is None: 
+    if user_result['email'] is None:
         response = make_response(
-            render_template('error.html', code=401, message='Cannot get your email from Github, your email must be visible in your Github account.'),
+            render_template(
+                'error.html',
+                code=401,
+                message='Cannot get your email from Github, \
+                    your email must be visible in your Github account.'),
             401
         )
         return response
 
-    ## Finally user is valid!
+    # Finally user is valid!
     login_session['name'] = user_result['name']
     login_session['email'] = user_result['email']
     login_session['picture'] = user_result['avatar_url']
@@ -263,6 +329,7 @@ def createUser(login_session):
     user = session.query(User).filter_by(email=login_session['email']).one()
     return user.id
 
+
 def getUserInfo():
     try:
         user_id = login_session['user_id']
@@ -271,6 +338,7 @@ def getUserInfo():
     except:
         return None
 
+
 def getUserID(email):
     try:
         user = session.query(User).filter_by(email=email).one()
@@ -278,10 +346,12 @@ def getUserID(email):
     except:
         return None
 
+
 def main():
     app.secret_key = 'my_secret_key'
     app.debug = True
     app.run(host='0.0.0.0', port=5001)
+
 
 if __name__ == '__main__':
     try:
